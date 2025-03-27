a5dbf94866631eb48605dacb884d25dcebd3fc99
iracic82 <iracic82@gmail.com>
2025-03-27 19:20:51 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -115,6 +115,28 @@ variable "North_US_AppSvcs_VNets" {
   }))
 }
 
+variable "GCP_EU_North" {
+  description = "Map of GCP instances and networking configuration per VPC"
+  type = map(object({
+    gcp_project            = string
+    gcp_region             = string
+    gcp_zone               = string
+    ssh_user               = string
+    startup_script         = string
+
+    gcp_vpc_name           = string
+    gcp_subnet_name        = string
+    gcp_private_ip         = string
+    gcp_app_fqdn           = string
+    gcp_instance_name      = string
+    gcp_vm_key_pair_name   = string
+    gcp_vpc_cidr           = string
+    gcp_subnet_cidr        = string
+
+    labels = map(string)
+  }))
+}
+
 variable "route53_domain_name" {
   description = "The domain name for the private hosted zone"
   type        = string
