d554eff54ca9aa7927fc9d5808c60898783c055b
iracic82 <iracic82@gmail.com>
2025-03-27 19:22:02 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,6 +93,30 @@ module "azure_instances_eu" {
 }
 
 
+# Create Linux and Networking Infrastructure in GCP
+
+module "gcp_instances" {
+  source   = "./modules/gcp-resources"
+  for_each = var.GCP_EU_North
+  providers = {
+    google = google.gcp_instances
+  }
+
+  project         = each.value["gcp_project"]
+  region          = each.value["gcp_region"]
+  zone            = each.value["gcp_zone"]
+
+  vpc_name        = each.value["gcp_vpc_name"]
+  subnet_name     = each.value["gcp_subnet_name"]
+  subnet_cidr     = each.value["gcp_subnet_cidr"]
+  private_ip      = each.value["gcp_private_ip"]
+  instance_name   = each.value["gcp_instance_name"]
+
+  startup_script = each.value["startup_script"] # just the string path
+  ssh_user        = each.value["ssh_user"]
+
+  labels          = each.value["labels"]
+}
 
 
 
