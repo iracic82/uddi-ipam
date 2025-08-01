e90e4718f75e26b70d73176b0fd259e1ae2d7e10
iracic82 <iracic82@gmail.com>
2025-08-01 11:00:15 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,7 +91,7 @@ module "azure_instances_eu" {
   azure_instance_name  = each.value["azure_instance_name"]
   azure_private_ip     = each.value["azure_private_ip"]
   azure_server_key_pair_name  = each.value["azure_server_key_pair_name"]
-  azure_vm_size        = "Standard_DS2_v3"
+  azure_vm_size        = "Standard_B4ms"
   azure_admin_username = "linuxuser"
   azure_admin_password = "admin123"
   enable_peering = each.value["enable_peering"]
